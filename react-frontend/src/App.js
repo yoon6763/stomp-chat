b1081db3ccc9d4b9d6b2c5bf30ff6e3d50e3e82e
전윤호 Uknow <yoon6763@naver.com>
2024-05-06 21:07:25 +0900

    feat: 채팅 기능 1차 구현

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import * as StompJs from '@stomp/stompjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function CreateReadChat() {
+    const [chatList, setChatList] = useState([]);
+    const [chat, setChat] = useState('');
+
+    const { apply_id } = useParams();
+    const client = useRef({});
+
+    const connect = () => {
+        client.current = new StompJs.Client({
+            brokerURL: 'ws://localhost:8080/ws',
+            onConnect: () => {
+                console.log('success');
+                subscribe();
+            },
+        });
+        client.current.activate();
+    };
+
+    const publish = (chat) => {
+        if (!client.current.connected) return;
+
+        client.current.publish({
+            destination: '/pub/chat',
+            body: JSON.stringify({
+                applyId: apply_id,
+                chat: chat,
+            }),
+        });
+
+        setChat('');
+    };
+
+    const subscribe = () => {
+        client.current.subscribe('/sub/chat/' + apply_id, (body) => {
+            const json_body = JSON.parse(body.body);
+            setChatList((_chat_list) => [
+                ..._chat_list, json_body
+            ]);
+        });
+    };
+
+    const disconnect = () => {
+        client.current.deactivate();
+    };
+
+    const handleChange = (event) => { // 채팅 입력 시 state에 값 설정
+        setChat(event.target.value);
+    };
+
+    const handleSubmit = (event, chat) => { // 보내기 버튼 눌렀을 때 publish
+        event.preventDefault();
+
+        publish(chat);
+    };
+
+    useEffect(() => {
+        connect();
+
+        return () => disconnect();
+    }, []);
+
+    return (
+        <div>
+            <div className={'chat-list'}>{chatList}</div>
+            <form onSubmit={(event) => handleSubmit(event, chat)}>
+                <div>
+                    <input type={'text'} name={'chatInput'} onChange={handleChange} value={chat} />
+                </div>
+                <input type={'submit'} value={'의견 보내기'} />
+            </form>
+        </div>
+    );
 }
 
-export default App;
+export default CreateReadChat;
--- spring-backend/src/main/java/com/example/demo/controller/WebSocketController.java
+++ spring-backend/src/main/java/com/example/demo/controller/WebSocketController.java
@@ -2,20 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.data.ChatDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class WebSocketController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/chat")
     public void sendMessage(ChatDto chatDto, SimpMessageHeaderAccessor accessor) {
+        log.info("chatDto: {}", chatDto);
         simpMessagingTemplate.convertAndSend("/sub/chat/" + chatDto.getChannelId(), chatDto);
     }
 }
